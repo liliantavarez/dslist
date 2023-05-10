5e96dc0e2cbd7d40dc9eb6dbb7d078e99442f72e
Lilian Carvalho <liliancarvalhotavares@gmail.com>
2023-05-10 10:17:05 -0300

    implementação de modelo de dominio

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Belonging other = (Belonging) obj;
+        return Objects.equals(id, other.id);
+    }
+}
---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPK.java
@@ -0,0 +1,35 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+import java.util.Objects;
+
+@Embeddable
+public class BelongingPK {
+
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList list;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        BelongingPK other = (BelongingPK) obj;
+        return Objects.equals(game, other.game) && Objects.equals(list, other.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(game, list);
+    }
+}
